00d1343edcede1b98bfbf2bf190f1b936567a6d1
Srijan Chatterjee <97849814+Srijan3004@users.noreply.github.com>
2024-01-26 20:37:51 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,7 +1,13 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $participant = $_GET['participant'];
 $judge1 = $_GET['judge1'];
 $judge2 = $_GET['judge2'];
+
+// ... rest of your code
+
 
 // Validate input (optional)
 // Add any additional validation logic here...
